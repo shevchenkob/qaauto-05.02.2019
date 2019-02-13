a5a84a75a9e280588f6e216503291db35ff44662
Bogdan Shevchenko <shevbv@gmail.com>
2019-02-13 18:22:22 +0200

    13.02.2019/2

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,18 +5,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginTests {
     @Test
-    public void successfullLoginTest() {
+    public void successfullLoginTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\shevchenko_b\\IdeaProjects\\qaauto-05.02.2019\\chromedriver.exe");
         System.setProperty("webdriver.chrome.silentLogging", "true");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/");
 
 //Here I write all elements that will be used
-        String useremail = "qwerty@gmail.com";
-        String userpassword = "mypassword";
+        String useremail = "johndoeseleniumtest@gmail.com";
+        String userpassword = "johndoepassword";
         WebElement SignIn = driver.findElement(By.id("login-submit"));
+
 
 //Here I write all methods
         WebElement emailfield = driver.findElement(By.id("login-email"));
@@ -24,6 +27,11 @@ public class LoginTests {
         emailfield.sendKeys(useremail);
         passwordfield.sendKeys(userpassword);
         SignIn.click();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(5000);
+        WebElement home = driver.findElement(By.id("feed-tab-icon"));
+        home.click();
+
 
     }
 }
