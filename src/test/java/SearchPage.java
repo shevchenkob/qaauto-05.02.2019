537111c3afa6d9827aa8385f72341db8335d27d5
Bogdan Shevchenko <shevbv@gmail.com>
2019-03-21 17:17:47 +0200

    21.03.2019/homework(without scroll)

--- src/test/java/SearchPage.java
+++ src/test/java/SearchPage.java
@@ -3,11 +3,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchPage {
     private WebDriver driver;
 
     @FindBy (xpath = "//h3[contains(@class, 'search-results__total')]")
     private WebElement resultsTotal;
+
+    @FindBy (xpath = "//div[@class='search-result__wrapper']")
+    private List<WebElement> searchBlock;
 
     public SearchPage(WebDriver driver) {
         this.driver = driver;
@@ -19,6 +25,19 @@ public class SearchPage {
         return  resultsTotal.isDisplayed()
                 && driver.getCurrentUrl().contains("/search/results/")
                 && driver.getTitle().contains(" | Search ");
+    }
+
+
+    public List<String> getSearchResultsList() {
+        List<String> searchListResultTexts = new ArrayList<String>();
+        for (WebElement  searchBlockText: searchBlock){
+             searchListResultTexts.add(searchBlockText.getText());
+        }
+        return searchListResultTexts;
+    }
+
+    public int getSearchResultCount () {
+        return searchBlock.size();
     }
 
 
--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,23 +9,25 @@ public class SearchTests extends BaseTest {
     @Test
     public void SearchTest() throws InterruptedException {
         String searchTerm = "HR";
+       // Integer getSearchResultCount ;
 
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded.");
         HomePage homePage = landingPage.login("johndoeseleniumtest@gmail.com", "johndoepassword");
         Thread.sleep(5000);
         Assert.assertTrue(homePage.isPageLoaded(), "Home page is not loaded.");
-
         SearchPage searchPage = homePage.search(searchTerm);
+        List<String> searchResultsList =  searchPage.getSearchResultsList();
+        Thread.sleep(5000);
+
         Assert.assertTrue(searchPage.isPageLoaded(), "Search page is not loaded.");
+        Integer getSearchResultCount ;
+        getSearchResultCount = searchResultsList.size();
 
         Assert.assertEquals(searchPage.getSearchResultCount(), 10, "Search results count is wrong.");
-
-        List<String> searchResultsList = searchPage.getSearchResultsList();
 
         for (String searchResult : searchResultsList) {
             Assert.assertTrue(searchResult.contains(searchTerm), "pls write this message(searchTerm not found in searchResultsList)");
         }
-
 
         }
 
